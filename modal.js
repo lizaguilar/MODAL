51a3e8c53f1073f97f68fc52f332bb811ee61430
elizabeth aguilar <elakeys@gmail.com>
2016-03-16 22:44:58 -0400

    modal hw

---
+++ modal.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  
+  $(".link").click(function(){
+    
+    $(".modal").show();
+      
+      $(".modal").click(function(){
+        
+        $(".modal").hide();
+    
+    });
+  
+  });
+
+});
+
